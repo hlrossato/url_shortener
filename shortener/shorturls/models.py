bd61ec96d85d9c111cdedf7f0bc5d43afce22226
Higor Rossato <higorleandrorossato@gmail.com>
2019-11-24 18:42:31 +0100

    Add UrlShortener model, tests and migrations

--- shortener/shorturls/models.py
+++ shortener/shorturls/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from shorturls.utils import generate_short_url, generate_url_short_code
 
 
 class UrlShortener(models.Model):
 
-    long_url = models.URLField("Long URL", max_length=200)
+    long_url = models.URLField("Long URL", max_length=200, unique=True, db_index=True)
     short_url = models.URLField("Short URL", max_length=200, blank=True)
+    url_short_code = models.SlugField("URL Short Code", unique=True, db_index=True)
 
     class Meta:
         verbose_name = "URL Shortener"
         verbose_name_plural = "URL Shortener"
+
+    def save(self, *args, **kwargs):
+        code = generate_url_short_code()
+        self.url_short_code = code
+        self.short_url = generate_short_url(code)
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.short_url
--- shortener/shorturls/tests/test_models.py
+++ shortener/shorturls/tests/test_models.py
@@ -5,11 +5,13 @@ from shorturls.tests.mixins import UrlShortenerTestCaseMixin
 
 class UrlShortenerTestCase(UrlShortenerTestCaseMixin, TestCase):
     def test_long_url_can_be_saved(self):
-        """Ensure a long url can be saved (still without generate the
-        short url version)
+        """Ensure a long url can be saved correctly and the short url is
+        generated
         """
         shortened_url = UrlShortener.objects.create(long_url=self.long_url)
         self.assertEqual(shortened_url.long_url, self.long_url)
-        self.assertEqual(shortened_url.short_url, "")
+        self.assertNotEqual(shortened_url.short_url, "")
+        self.assertNotEqual(shortened_url.url_short_code, "")
         self.assertEqual(UrlShortener.objects.count(), 1)
         self.assertIsInstance(shortened_url, UrlShortener)
+        self.assertEqual(shortened_url.__str__(), shortened_url.short_url)
